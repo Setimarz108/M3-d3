d057911cbe6f6a551468ed0dee5b69e4f1eaa142
Setimarz108 <66173614+Setimarz108@users.noreply.github.com>
2021-12-02 15:39:45 +0100

    refactor code

--- M3-d4_books/app.js
+++ M3-d4_books/app.js
@@ -8,29 +8,42 @@ const loadBooks = () => {
 
         console.log(books)
 
-        const row = document.querySelector(".row")
+      
 
         books.forEach(book => {
 
-            const col = document.createElement("div")
-                        col.className = "col-3"
-
-                        col.innerHTML = `
-                        <div class="card">
-                            <img src=${book.img} class="card-img-top" alt=${book.title} image>
-                            <div class="card-body">
-                                <h5 class="card-title">${book.title}</h5>
-                                <p class="card-text">${book.category}</p>
-                                <a href="#" class="btn btn-primary">${book.price}</a>
-                            </div>
-                        </div>
-                        `
-                            row.appendChild(col)
+            create_cards(book);
+                    
+                           
+                
             
         });
 
 
     })
+}
+
+
+const create_cards = (book) =>{  
+    
+    const row = document.querySelector(".row")
+       const col = document.createElement("div")
+        col.className = "col-3"
+
+        col.innerHTML = `
+             <div class="card">
+                <img src=${book.img} class="card-img-top" alt=${book.title} image>
+                 <div class="card-body">
+                       <h5 class="card-title">${book.title}</h5>
+                        <p class="card-text">${book.category}</p>
+                        <a href="#" class="btn btn-primary">${book.price}</a>
+                <button type="button" class="btn btn-dark">Add to <i class="bi bi-cart2 text-white"></i> </button>
+               </div>
+                  </div> `
+
+        row.appendChild(col)
+
+
 }
 
 loadBooks()
